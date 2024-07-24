0f7c595ef47e24e3a638716b451e83b26c62778d
Liehan Els <ST10085345@vcconnect.edu.za>
2024-07-24 14:09:08 +0200

    Great progress so far!

--- SkerTinikor/Form1.Designer.cs
+++ SkerTinikor/Form1.Designer.cs
@@ -631,6 +631,7 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "A1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // mainScreen
             // 
--- SkerTinikor/Form1.cs
+++ SkerTinikor/Form1.cs
@@ -7,14 +7,101 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using Microsoft.VisualBasic.ApplicationServices;
 
 namespace SkerTinikor
 {
     public partial class mainScreen : Form
     {
+        string[,] seats = new string[5,10];
         public mainScreen()
         {
             InitializeComponent();
+            initSeats(seats);
+        }
+        public void initSeats(string[,] s)
+        {
+            for (int row = 0; row < 5; row++)
+            {
+                for (int col = 0; col < 10; col++)
+                {
+                    s[row, col] = "*";
+                }
+            }
+        }
+        public int getX(char xS)
+        {
+            int x = 0;
+            switch (xS)
+            {
+                case 'A':
+                    x = 0;
+                    break;
+                case 'B':
+                    x = 1;
+                    break;
+                case 'C':
+                    x = 2;
+                    break;
+                case 'D':
+                    x = 3;
+                    break;
+                case 'E':
+                    x = 4;
+                    break;
+                case 'F':
+                    x = 5;
+                    break;
+                case 'G':
+                    break;
+                    x = 6;
+                case 'H':
+                    x = 7;
+                    break;
+                case 'I':
+                    x = 8;
+                    break;
+                case 'J':
+                    x = 9;
+                    break;
+            }
+                return x;
+        }
+        public int getY(char yS)
+        {
+            int y = int.Parse(yS + "") - 1;
+            return y;
+        }
+        public bool isSeatBooked(string[,] s, int x, int y)
+        {
+            if (s[y, x].Equals("*")) { return false; }
+            else 
+            {
+                MessageBox.Show("Seat is booked by " + s[y,x]);
+                DialogResult unbook = MessageBox.Show("Unbook this seat?", "Confirm Unbooking", MessageBoxButtons.YesNo);
+                if (unbook == DialogResult.Yes)
+                {
+                    unbookSeat(s, x, y);
+                    bookSeat(s, x, y);
+                }
+                return true;
+            }
+        }
+        public void bookSeat(string[,] s, int x, int y)
+        {
+                s[y,x] = Interaction.InputBox("Enter your name");
+        }
+        public void unbookSeat(string[,] s, int x, int y)
+        {
+            s[y, x] = "*";
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!isSeatBooked(seats, getX(button1.Text[0]), getY(button1.Text[1])))
+            {
+                bookSeat(seats, getX(button1.Text[0]), getY(button1.Text[1]));
+            }
         }
     }
 }
